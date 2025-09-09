fc7966b97c8efe8b59bd7432046642aec47305db
Dusan Perkovic <pljeskavicica@gmail.com>
2025-09-09 03:05:24 +0200

    Add run scripts to root package.json

--- client/src/routes/login.tsx
+++ client/src/routes/login.tsx
@@ -1,9 +1,19 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { useEffect } from 'react'
 
 export const Route = createFileRoute('/login')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
+  const fetchApi = async () => {
+    const response = await fetch("http://localhost:8080/api").then(res => res.json())
+    console.log(response)
+  }
+  useEffect(() => {
+    fetchApi();
+  }, [])
+
+
   return <div>Hello "/login"!</div>
 }
